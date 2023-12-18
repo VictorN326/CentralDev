162ff5efeda6f354e464799e6932c10b58574e91
VictorN326 <21vnguyen3@gmail.com>
2023-12-18 10:01:32 -0800

    Added Light/Dark mode and worked on types file

--- centraldev/components/shared/navbar/Navbar.tsx
+++ centraldev/components/shared/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import Theme from "./Theme";
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -18,7 +19,7 @@ const Navbar = () => {
       </Link>
       {/* GlobalSearch goes here */}
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
