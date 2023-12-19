a0e880ce060afe9e83d0e4e0b8a52738c8c13748
VictorN326 <21vnguyen3@gmail.com>
2023-12-18 20:52:38 -0800

    Did RightSidebar

--- centraldev/app/(root)/(home)/page.tsx
+++ centraldev/app/(root)/(home)/page.tsx
@@ -1,5 +1,9 @@
 import { UserButton } from "@clerk/nextjs";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <div className="h-screen"></div>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
