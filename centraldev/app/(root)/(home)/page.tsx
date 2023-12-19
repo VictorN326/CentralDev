f45fdba3e7d074136e7bbf52eec140a4f8203faa
VictorN326 <21vnguyen3@gmail.com>
2023-12-19 14:13:07 -0800

    Added HomePage Filters. Full Functionality added later

--- centraldev/app/(root)/(home)/page.tsx
+++ centraldev/app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import LocalSearchBar from "@/components/shared/search/LocalSearchbar";
+import Filter from "@/components/shared/Filter";
+import { HomePageFilters } from "@/constants/filters";
+import HomeFilters from "@/components/home/HomeFilters";
 export default function Home() {
   return (
     <>
@@ -23,8 +25,13 @@ export default function Home() {
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        Filters here
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+      <HomeFilters />
     </>
   );
 }
--- centraldev/components/shared/navbar/Theme.tsx
+++ centraldev/components/shared/navbar/Theme.tsx
@@ -34,7 +34,7 @@ const Theme = () => {
             />
           )}
         </MenubarTrigger>
-        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark: border-dark-400 dark:bg-dark-300 ">
+        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 bg-light-900 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((theme) => (
             <MenubarItem
               className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
