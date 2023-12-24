f657fcb04b31037ebf401f6b9f3ddd0200553574
VictorN326 <21vnguyen3@gmail.com>
2023-12-23 19:51:30 -0800

    Finalized Webhooks Setup

--- centraldev/app/(root)/ask-question/page.tsx
+++ centraldev/app/(root)/ask-question/page.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import Question from "@/components/forms/Question";
 import { redirect } from "next/navigation";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 const page = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  const userId = "clrk123";
   if (!userId) {
     redirect("/sign-in");
   }
 
   const mongodbUser = await getUserById({ userId });
 
-  console.log("DEBUG: mongodbUser: ", mongodbUser);
+  // console.log("DEBUG: mongodbUser: ", mongodbUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
