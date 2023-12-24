fe8276704e9ab158a24766e5008c1f18fd2f49bc
VictorN326 <21vnguyen3@gmail.com>
2023-12-23 18:41:51 -0800

    Added middleware routes and fixed signin/signup

--- centraldev/app/(auth)/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignIn = () => {
-  return <h1 className="h1-bold">Sign In</h1>;
-};
-
-export default SignIn;
--- centraldev/app/api/webhook/route.ts
+++ centraldev/app/api/webhook/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
 
   //TODO: Add your webhook secret to .env.local
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -55,6 +55,7 @@ export async function POST(req: Request) {
   // eventType are different user events such as user.created, user.updated, user.deleted
   const eventType = evt.type;
 
+  console.log({ eventType });
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
