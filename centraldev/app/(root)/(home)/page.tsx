ac8615d05abdf3e703bf13bb5aa54dfefef8cc2c
VictorN326 <21vnguyen3@gmail.com>
2023-12-23 18:09:23 -0800

    Fixed undefined errors

--- centraldev/app/(root)/(home)/page.tsx
+++ centraldev/app/(root)/(home)/page.tsx
@@ -41,8 +41,8 @@ export default async function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {result?.questions?.length > 0 ? (
-          result?.questions.map((question) => (
+        {result && result.questions && result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
