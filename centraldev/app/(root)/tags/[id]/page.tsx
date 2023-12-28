c90f81a6d8e1f5b258063263a787cd30a25ca750
VictorN326 <21vnguyen3@gmail.com>
2023-12-27 18:56:53 -0800

    Implemented Tag Details

--- centraldev/app/(root)/tags/[id]/page.tsx
+++ centraldev/app/(root)/tags/[id]/page.tsx
@@ -1,46 +1,45 @@
-import LocalSearchBar from "@/components/shared/search/LocalSearchbar";
-import Filter from "@/components/shared/Filter";
-import { QuestionFilters } from "@/constants/filters";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import React from "react";
 import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
-import { getSavedQuestions } from "@/lib/actions/user.action";
-import { auth } from "@clerk/nextjs";
-export default async function Collection() {
-  const { userId } = auth();
-
-  if (!userId) return null;
-  const result = await getSavedQuestions({
-    clerkId: userId,
+import LocalSearchBar from "@/components/shared/search/LocalSearchbar";
+import { IQuestion } from "@/database/question.model";
+import { URLProps } from "@/types";
+const TagDetails = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
   });
 
-  // console.log("DEBUG: result: ", result?.questions);
+  console.log("DEBUG: result: ", result);
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <h1 className="h1-bold text-dark100_light900">{result?.tagTitle}</h1>
 
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <div className="mt-11 w-full ">
         <LocalSearchBar
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search for questions"
+          placeholder="Search tag questions"
           otherClasses="flex-1"
-        />
-        <Filter
-          filters={QuestionFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {result && result.questions && result.questions.length > 0 ? (
-          result.questions.map((question: any) => (
+          result.questions.map((question: IQuestion) => (
+            //TODO: Fix the types for tag, author, and upVotes later due to IQuestion type
             <QuestionCard
               key={question._id}
               _id={question._id}
               title={question.title}
+              //@ts-ignore
               tags={question.tags}
+              //@ts-ignore
               author={question.author}
+              //@ts-ignore
               upVotes={question.upVotes}
               answers={question.answers}
               views={question.views}
@@ -58,4 +57,6 @@ export default async function Collection() {
       </div>
     </>
   );
-}
+};
+
+export default TagDetails;
